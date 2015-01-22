e110e66cab5bffa1a441950606a19950e46dc7e8
Nipam Patel <nipam2000@gmail.com>
2015-01-22 13:34:25 -0500

    First commit

---
+++ MVCActiveDirectoryAuthentication.Services/Helpers/AuthenticationHelper.cs
@@ -0,0 +1,32 @@
+﻿using MvcActiveDirectoryAuthentication.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.DirectoryServices;
+using System.Linq;
+using System.Web;
+
+namespace MVCActiveDirectoryAuthentication.Services
+{
+    public class AuthenticationHelper
+    {
+        public static readonly string LDAP_CONNECTION_STRING = ConfigurationManager.AppSettings["LDAPConnection"];
+        public static bool AuthenticateUser(LoginModel model)
+        {
+            DirectoryEntry directoryEntry = new DirectoryEntry(AuthenticationHelper.LDAP_CONNECTION_STRING);
+            directoryEntry.Username = model.UserName;
+            directoryEntry.Password = model.Password;
+
+            try
+            {
+                var directorySearcher = new DirectorySearcher(directoryEntry);
+                SearchResult foundRecord = directorySearcher.FindOne();
+            }
+            catch(DirectoryServicesCOMException authException)
+            {
+                return false;    
+            }            
+            return true;
+        }
+    }
+}
